1dc42f1fcbd5f21f706cf57a8bf0590dab696c1f
akshad2007 <akshadb272gmail.com>
2026-02-27 14:54:34 +0530

    added

---
+++ uploads/middleware/upload.js
@@ -0,0 +1,36 @@
+const multer = require("multer");
+const path = require("path");
+
+// Storage configuration
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const uniqueName =
+      Date.now() + "-" + file.originalname.replace(/\s+/g, "");
+    cb(null, uniqueName);
+  }
+});
+
+// File filter (only images)
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = /jpeg|jpg|png/;
+  const extName = allowedTypes.test(
+    path.extname(file.originalname).toLowerCase()
+  );
+  const mimeType = allowedTypes.test(file.mimetype);
+
+  if (extName && mimeType) {
+    cb(null, true);
+  } else {
+    cb("Only images (jpg, jpeg, png) are allowed");
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter
+});
+
+module.exports = upload;
